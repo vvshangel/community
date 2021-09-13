c61e15bc56ae2f072b238ce38860e4ccc670c579
vvshangel <981764577@qq.com>
2021-09-13 15:51:49 +0800

    首次提交

--- src/test/java/com/example/community/CommunityApplicationTests.java
+++ src/test/java/com/example/community/CommunityApplicationTests.java
@@ -5,6 +5,7 @@ import com.example.community.Mapper.DiscussPostMapper;
 import com.example.community.Mapper.UserMapper;
 import com.example.community.entity.DiscussPost;
 import com.example.community.entity.User;
+import com.example.community.util.MailClient;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.List;
 
@@ -58,6 +61,30 @@ class CommunityApplicationTests {
         logger.error("error log");
         logger.info("info log");
         logger.warn("warn log");
+    }
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testMail(){
+        mailClient.sendMail("981764577@qq.com","hello","歪比歪比");
+    }
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","hello");
+
+        String content = templateEngine.process("/templates/demo", context);
+
+        System.out.println(content);
+
+        mailClient.sendMail("981764577@qq.com","hello",content);
+
     }
 
 
